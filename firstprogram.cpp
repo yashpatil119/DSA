45e3b61eaad3ba515a12b88c6594289fbd84dc85
yashpatil119 <115876194+yashpatil119@users.noreply.github.com>
2023-07-17 04:39:45 -0700

    Add files via upload

---
+++ firstprogram.cpp
@@ -0,0 +1,7 @@
+#include<iostream>//header file for input & output
+using namespace std;
+int main(){// main in the name of fn i.e Entry point of program & int is return type of fn 
+    cout<<"hello world"<<endl;
+
+    return 0;// it means our program is sucessfully terminmated.
+}
