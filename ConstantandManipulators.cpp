e9f4b255ab17cc0e9f35f0b5aba9ae506874db06
yashpatil119 <115876194+yashpatil119@users.noreply.github.com>
2023-07-26 11:32:36 +0530

    Add files via upload

---
+++ ConstantandManipulators.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<iomanip>
+using namespace std ;
+
+int main()
+{
+   /* int a = 3.14;
+
+     cout<<"The value of a is :"<<a<<endl;
+     a=45; //here value of a is changed to solve this problem we use constant keyword
+     cout<<"The value of a is :"<<a<<endl;*/
+
+     //*****************CONSTANT IN C++************************//
+
+   /* const float a=3.14;
+
+     cout<<"The value of a is ="<<a<<endl;
+     a=45.6;//You will ger an error because a is constant
+     cout<<"The value of a is ="<<a<<endl;*/
+
+
+
+
+     //**********-------<<<MANIPULATORS IN C++>>>>>---------**************
+
+     int a=5,b=89,c=1333;
+
+     cout<<"The Value of a Without Set W ="<<a<<endl;
+      cout<<"The Value of a Without Set W ="<<b<<endl;
+       cout<<"The Value of a Without Set W ="<<c<<endl;
+
+
+        cout<<"The Value of a With Set W ="<<setw(4)<<a<<endl;
+         cout<<"The Value of a With Set W ="<<setw(4)<<b<<endl;
+          cout<<"The Value of a With Set W ="<<setw(4)<<c<<endl;
+
+
+
+}
