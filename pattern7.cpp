cc9787e3a9054443a2b284445dd5b572be8ebce9
yashpatil119 <115876194+yashpatil119@users.noreply.github.com>
2023-08-15 22:53:02 +0530

    Add files via upload

---
+++ pattern7.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print7(int n)
+ {
+
+ for(int i=0;i<=n;i++)
+  {
+ 
+    //for space 
+    for(int j=0;j<n-i-1;j++)
+    {
+        cout<<" ";
+    }
+
+    //for star
+    for(int j=0;j<2*i-1;j++)
+    {
+    cout<<"*";
+    }
+    //for space
+    for(int j=0;j<n-i+1;j++)
+    {
+        cout<<" ";
+
+    }
+    cout<<endl;
+
+ }
+ }
+
+ int main(){
+    int n;
+    cin>>n;
+    print7(n);
+    return 0;
+    
+ }
