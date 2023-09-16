e2ab5fec1bfae2bf1012c63bbab164c86191422a
yashpatil119 <115876194+yashpatil119@users.noreply.github.com>
2023-09-15 20:19:39 -0700

    Add files via upload

---
+++ patterns/pattern3new.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using nampespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+
+    for(int row=0;row<n;row++)
+    {
+        //for space
+        for(int col=0;col<n-row-1;col++)
+        {
+            cout<<" ";
+        }
+        //for star
+        for(int col=0;col<row+1;col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+     for(int row=0;row<n;row++)
+    {
+        
+        
+        //for space
+        for(int col=0;col<row;col++)
+        {
+            cout<<" ";
+        }
+
+        //for star
+        for(int col=0;col<n-row;col++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+        
+    }
+
+
+}
